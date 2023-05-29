7a227ebdaa83d27a3a834cb7bc2535c765ed97af
prashansarai <prashansaraiitdelhi2019@gmail.com>
2023-05-29 10:12:44 +0530

    adding second file

---
+++ dom.js
@@ -0,0 +1,5 @@
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = 'GoodBye';
+headerTitle.innerHTML = '<h3>Hello</h3>';
