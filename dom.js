ec17bec2941c0354dc972091891f5dcfba3de600
prashansarai <prashansaraiitdelhi2019@gmail.com>
2023-05-29 15:45:36 +0530

    more changes

--- dom.js
+++ dom.js
@@ -6,5 +6,14 @@ headerTitle.innerHTML = '<h3>Hello</h3>';
 var items = document.getElementsByClassName('list-group-items');
 console.log(items);
 console.log(items[1]);
-items.style.fontWeight = 'bold';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
 items[3].style.backgroundColor = 'green';
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight = 'italics';
+}
+li[3].style.backgroundColor = 'yellow';
