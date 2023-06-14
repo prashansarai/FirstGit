706dc5919592a62a01d7843905f214fff3fe8159
prashansarai <prashansaraiitdelhi2019@gmail.com>
2023-06-14 10:30:56 +0530

    adding description

--- main.js
+++ main.js
@@ -7,9 +7,10 @@ itemList.addEventListener('click',removeItem);
 function addItem(e){
     e.preventDefault();
     var newItem = document.getElementById('item').value;
+    var newDscrpn = document.getElementById('description').value;
     var li = document.createElement('li');
     li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newItem + " "+newDscrpn));
     var deleteBtn = document.createElement('button');
     deleteBtn.className = "btn btn-danger btn-sm float-right delete";
     deleteBtn.appendChild(document.createTextNode('X'));
@@ -29,18 +30,20 @@ function filterItems(e){
     var items = itemList.getElementsByTagName('li');
     Array.from(items).forEach(function(item){
         var itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) != -1){
+        var dscp = item.childNodes[1].textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1 && dscp.toLowerCase().indexOf(text)!=-1){
             item.style.display = 'block';
         }
         else{
             item.style.display = 'none';
         }
+
     });
 } 
-var li = document.getElementsByTagName('li');
-var editBtn = document.createElement('button');
-// editBtn.classList.add('edit');
-editBtn.appendChild(document.createTextNode('E'));
-li.appendChild(editBtn);
+// var li = document.getElementsByTagName('li');
+// var editBtn = document.createElement('button');
+// // editBtn.classList.add('edit');
+// editBtn.appendChild(document.createTextNode('E'));
+// li.appendChild(editBtn);
 
 
