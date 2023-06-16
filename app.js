2a5ae102bc77b8f92c5df016a375030f76f265e5
Prashansa Rai <65287608+prashansarai@users.noreply.github.com>
2023-06-16 08:30:34 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('I love git')
+var nameText = document.getElementById('name');
+var emailText = document.getElementById('email');
+var form = document.getElementById('my-form');
+form.addEventListener('submit', storage);
+function storage(e){
+    e.preventDefault();
+    localStorage.setItem('Name', nameText.value);
+    console.log(localStorage.getItem('Name'));
+    localStorage.setItem('Email', emailText.value);
+    console.log(localStorage.getItem('Email'));
+}
