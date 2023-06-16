2254f58473da7be8d43dde5d57ef22e3e1310ecb
Prashansa Rai <65287608+prashansarai@users.noreply.github.com>
2023-06-16 10:09:04 +0530

    multiple user details local storage

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ var form = document.getElementById('my-form');
 form.addEventListener('submit', storage);
 function storage(e){
     e.preventDefault();
-    localStorage.setItem('Name', nameText.value);
-    console.log(localStorage.getItem('Name'));
-    localStorage.setItem('Email', emailText.value);
-    console.log(localStorage.getItem('Email'));
+    var my_obj={
+        'Name': nameText.value,
+        'Email': emailText.value
+    };
+    var obj_serialised = JSON.stringify(my_obj);
+    localStorage.setItem(nameText.value, obj_serialised);
+    console.log(localStorage.getItem(nameText.value));
+    // localStorage.setItem('Name', nameText.value);
+    // console.log(localStorage.getItem('Name'));
+    // localStorage.setItem('Email', emailText.value);
+    // console.log(localStorage.getItem('Email'));
 }
-var my_obj={
-    Name :'Evana',
-    Age:'29'
-};
-var obj_serialised = JSON.stringify(my_obj);
-localStorage.setItem('Details', obj_serialised);
-console.log(localStorage.getItem('Details'));
+
