faa27b5789e6c27c63611ff674db23eac7dd5d56
Prashansa Rai <65287608+prashansarai@users.noreply.github.com>
2023-06-16 09:37:10 +0530

    adding details in local storage

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('I love git')
+var nameText = document.getElementById('name');
+var emailText = document.getElementById('email');
+var form = document.getElementById('my-form');
+form.addEventListener('submit', storage);
+function storage(e){
+    e.preventDefault();
+    localStorage.setItem('Name', nameText.value);
+    console.log(localStorage.getItem('Name'));
+    localStorage.setItem('Email', emailText.value);
+    console.log(localStorage.getItem('Email'));
+}
+var my_obj={
+    Name :'Evana',
+    Age:'29'
+};
+var obj_serialised = JSON.stringify(my_obj);
+localStorage.setItem('Details', obj_serialised);
+console.log(localStorage.getItem('Details'));
